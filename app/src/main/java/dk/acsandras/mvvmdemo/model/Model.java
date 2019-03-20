045a32ad62475b3c8d0814a45cd372b4e80b27a0
sainsh <kfsainsh@gmail.com>
2019-03-20 10:08:57 +0100

    removed mutableLiveDate from Model added changeA(string a) method to Model, changeA set a to a string value, and notifies observers

--- app/src/main/java/dk/acsandras/mvvmdemo/model/Model.java
+++ app/src/main/java/dk/acsandras/mvvmdemo/model/Model.java
@@ -1,29 +1,35 @@
 package dk.acsandras.mvvmdemo.model;
 
-import androidx.lifecycle.MutableLiveData;
+import java.util.Observable;
 
-public class Model {
+
+
+public class Model extends Observable {
 
     // TODO (2) Konverter dine datafelter fra primitive variable som f.eks. double til LiveData
     // Det vil gøre, at denne data member vil sende nootifikationer til alle de klasser, som observerer
     // dette felt, og de klasser vil så kunne opdatere data automatiskt.
     // Se dette forklaret i https://developer.android.com/topic/libraries/architecture/livedata
-    private MutableLiveData<String> a = new MutableLiveData<>(); // Før var det "private String a;"
+    private String a; // Før var det "private String a;"
 
     // TODO (3) Lav getters og setters og evt. en constructor for din data
     public Model() {
         this.setA("Startdata fra modellen");
     }
 
-    public MutableLiveData<String> getA() {
-        if (a == null) {
-            a = new MutableLiveData<String>();
-        }
+    public String getA() {
+
         return a;
     }
 
+    public void changeA(String a) {
+        this.a = a;
+        setChanged();
+        notifyObservers(a);
+    }
+
     public void setA(String a) {
-        this.a.setValue(a);
+        this.a = a;
     }
 
 }
